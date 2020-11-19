62a1de9177e1f7e38a3e68f22d066652b3537349
Matt Smith <mrandolph83@gmail.com>
2020-11-19 16:56:32 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,8 +1,11 @@
 class Teacher < User
 
+  # Class Constant
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  def teach(KNOWLEDGE)
+  def teach
+    # Class Constant writes out the same way as @@all, not
+    # like an instance variable
     KNOWLEDGE.sample
   end
   
